f470456c9a0f5f19827ff8d36a0b74f50a086eba
unknown <kanagasiva0019@gmail.com>
2021-11-03 19:06:29 +0530

    mm

---
+++ server.js
@@ -0,0 +1,10 @@
+const express =require('express');
+const path = require('path');
+ 
+const app = express();
+app.use(express.static(_dirname + '/dist/recp-app'));
+app.get('/*', function(req,res){
+    res.sendFile(path.join(__dirname+'/dist/recp-app/index.html'));
+
+});
+app.listen(process.env.PORT);
